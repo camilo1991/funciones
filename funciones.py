c5c238e6297643d360eaf62e774e7787daf7c8de
Andrey Camilo Pena Rodriguez <andreycamilop@hotmail.com>
2022-06-29 21:10:34 -0500

    La primera función de pepito septiembre

--- funciones.py
+++ funciones.py
@@ -4,9 +4,17 @@ os.system("cls")
 #def nombre(): #Mi primera función
 #    pass
 
-def resta()
+def resta():
     a=2
     b=1
     c=a-b
     print("El resultado de la resta es: " , c)
 resta()
+
+
+def multi():
+    a=4
+    b=8
+    c=a*b
+    print("El resultado de la multiplicacion es:", c)
+multi()
